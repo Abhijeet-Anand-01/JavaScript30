3669b32ace6717e27a3bf44e03f84f6553662013
Abhijeet Anand <abhijeetad500@gmail.com>
2021-05-25 01:24:16 +0530

    Touch function added

--- js/Drum Kit.js
+++ js/Drum Kit.js
@@ -1,18 +1,23 @@
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-}
+// function removeTransition(e) {
+//     if (e.propertyName !== 'transform') return;
+//     e.target.classList.remove('playing');
+// }
 
 function playSound(e) {
+    console.log(e)
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
     if (!audio) return;
 
     key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
+    setTimeout(() => {
+        key.classList.remove('playing')
+    }, 200);
 }
 
 const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+// keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
