a06dd5610baf521171fd564e083035712d4d869a
Abhijeet-Anand-01 <abhijeetad500@gmail.com>
2022-02-13 14:53:51 +0530

    [Enhancement] Code refactor

--- js/Drum Kit.js
+++ js/Drum Kit.js
@@ -4,8 +4,9 @@
 // }
 
 function playSound(e) {
+    console.log(e)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
 
     if (!audio) return;
 
