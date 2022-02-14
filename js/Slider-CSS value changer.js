1aa3fcf11d86312b2e5a2ca8c0c746e8d4a69e9c
Abhijeet-Anand-01 <abhijeetad500@gmail.com>
2022-02-14 21:45:04 +0530

    code refactor

--- js/Slider-CSS value changer.js
+++ js/Slider-CSS value changer.js
@@ -1,7 +1,7 @@
 const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
-    const suffix = this.dataset.sizing || '';
+    const suffix = this.dataset.sizing || ''; 
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
